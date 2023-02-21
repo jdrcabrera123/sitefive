d63ddd9d15dcb5b6d73262fc5cbf7f615bdaed1f
jdrcabrera <jd.rcabrera@gmail.com>
2023-02-20 19:01:05 -0500

    responsive added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const loginBtn = document.querySelector('.login');
 const signup = document.querySelector('.signup');
 const short__icon = document.querySelector('.short__icon')
 const left__arrow = document.querySelector('.left__arrow')
+const signUpBtnBigScreen = document.querySelector('.signUpBtnBigScreen')
+const login = document.querySelector('.login')
+const yellowShapeBg = document.querySelector('.yellowShapeBg')
+const paragraphOne = document.querySelector('.paragraphOne')
 
 
 signup_btn.addEventListener('click', (e)=>{
@@ -16,4 +20,19 @@ left__arrow.addEventListener('click', ()=>{
     loginBtn.classList.remove('active')
     signup.classList.remove('active')
     short__icon.classList.remove('active')
+    yellowShapeBg.classList.remove('active')
+    signUpBtnBigScreen.classList.remove('active')
+    paragraphOne.classList.remove('active')
 })
+
+signUpBtnBigScreen.addEventListener('click', (e)=>{
+    e.preventDefault()
+    login.classList.add('active')
+    yellowShapeBg.classList.add('active')
+    short__icon.classList.add('active')
+    signup.classList.add('active')
+    signUpBtnBigScreen.classList.add('active')
+    paragraphOne.classList.add('active')
+    
+})
+
